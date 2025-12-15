eb90053f0b243d01d0fb34d1b80e9e74985d84f7
EC2 Default User <ec2-user@ip-172-31-13-174.ap-south-2.compute.internal>
2025-12-15 06:00:31 +0000

    initial e-commerce app setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to E-Commerce Application v1");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
