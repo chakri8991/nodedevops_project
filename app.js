d8b3abe561e009044f4109b553b0aa9f33daabbe
chakri8991 <chakravarthi1223@gmail.com>
2025-12-17 03:42:03 +0000

    Resolved merge conflict in GitOps deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to E-Commerce Application v1");
+  res.send("Welcome to E-Commerce Application v3");
 });
 
 app.listen(3000, () => {
